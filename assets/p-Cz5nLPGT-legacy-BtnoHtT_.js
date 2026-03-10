43763253081bd1b47fca2649394ef7d8d9823d99
Suphathinee <Suphathinee.s@kkumail.com>
2026-03-10 17:49:18 +0700

    Updates

--- assets/p-Cz5nLPGT-legacy-BtnoHtT_.js
+++ assets/p-Cz5nLPGT-legacy-BtnoHtT_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CvLJyo9g.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
+System.register(["./index-legacy-CDJ_BQ0h.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
