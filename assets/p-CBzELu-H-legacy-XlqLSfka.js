4ac40f185d6427f86a6b3367002e6ddb6265ad3d
Suphathinee <Suphathinee.s@kkumail.com>
2026-03-10 20:52:16 +0700

    Updates

--- assets/p-CBzELu-H-legacy-XlqLSfka.js
+++ assets/p-CBzELu-H-legacy-XlqLSfka.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ByanyMwK.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.q,n=t.x}],execute:function(){
+System.register(["./index-legacy-9vLluUcw.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.q,n=t.x}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
--- assets/p-D6Ynv7Xh-legacy-D6c2SLob.js
+++ assets/p-D6Ynv7Xh-legacy-D6c2SLob.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ByanyMwK.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.t}],execute:function(){
+System.register(["./index-legacy-9vLluUcw.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.t}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
