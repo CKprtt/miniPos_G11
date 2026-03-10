99947d3cd7741484f9f059945fbf43ac698c32f2
suphathineeso <suphathinee.s@kkumail.com>
2026-03-10 18:15:38 +0700

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,8 +5,9 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAZvaa6AlpRLcfvFbq1iT7gjYPw20aFwOM",
-  authDomain: "minipos2026.firebaseapp.com",
+  authDomain: "ckprtt.github.io",
   projectId: "minipos2026",
+  
   storageBucket: "minipos2026.firebasestorage.app",
   messagingSenderId: "467347591070",
   appId: "1:467347591070:web:58c137f4c6c0346fb5d08f"
