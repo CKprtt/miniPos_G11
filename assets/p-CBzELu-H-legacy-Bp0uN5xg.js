6a6cbf28a264707080fee4c5991bd7fba228e145
Suphathinee <Suphathinee.s@kkumail.com>
2026-03-10 08:49:08 +0700

    Updates

--- assets/p-CBzELu-H-legacy-Bp0uN5xg.js
+++ assets/p-CBzELu-H-legacy-Bp0uN5xg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-FoRidpl-.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.q,n=t.x}],execute:function(){
+System.register(["./index-legacy-DsXYDzr6.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.q,n=t.x}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
